25467baf732eb8f7d3523105f281f8c3b9f561d6
Daniel B <dantheman41@live.com.au>
2023-01-16 13:38:49 +1100

    Prevent concurrency in prebuild actions

--- kfile.js
+++ kfile.js
@@ -1,30 +1,30 @@
-let project = new Project('Car');
+const project = new Project('Car');
 
 // TODO: Windows CI builds, flatc compilation in CI
 "https://github.com/google/flatbuffers/releases/latest/download/Windows.flatc.binary.zip"
 
-var exec = require('child_process').exec;
-exec('flatc -c --scoped-enums --filename-suffix Generated --cpp-field-case-style lower --warnings-as-errors -o ./Sources/Game/Net/ ./protocol/*', function callback(error, stdout, stderr) {
-	if (error) {
-		console.log('flatc error:', error, stdout, stderr);
-	} else {
-		console.log('flatc executed.', stdout, stderr);
-	}
-});
-exec('./fmt', function callback(error, stdout, stderr) {
-	if (error) {
-		console.log('fmt error:', error, stdout, stderr);
-	} else {
-		console.log('fmt executed.', stdout, stderr);
-	}
-});
-exec('./installLibs.sh', function callback(error, stdout, stderr) {
-	if (error) {
-		console.log('Lib installation error:', error, stdout, stderr);
-	} else {
-		console.log('Lib installation done.', stdout, stderr);
-	}
-});
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const { stdout, stderr } = await exec('flatc -c --scoped-enums --filename-suffix Generated --cpp-field-case-style lower --warnings-as-errors -o ./Sources/Game/Net/ ./protocol/*');
+if (stderr) {
+	console.log('flatc error:', stdout, stderr);
+} else {
+	console.log('flatc executed.', stdout, stderr);
+}
+await exec('./fmt');
+if (stderr) {
+	console.log('fmt error:', stdout, stderr);
+} else {
+	console.log('fmt executed.', stdout, stderr);
+}
+
+await exec('./installLibs.sh');
+if (stderr) {
+	console.log('Lib installation error:', stdout, stderr);
+} else {
+	console.log('Lib installation done.', stdout, stderr);
+}
 
 project.cpp = true;
 
